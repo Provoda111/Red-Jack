81fe49066827a3a0e0a8a2d4aece03c19c998bc2
s2200790 <s2200790@edu.bc.fi>
2024-09-27 11:54:22 +0300

    Effects

---
+++ Red-Jack/Assets/Scripts/ParticleController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    public ParticleSystem particleSystem;
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+     //   if (Input.GetKeyUp("a"))
+       // {
+         //   particleSystem.Play();
+           // Debug.Log("Firee");
+            
+        //}
+    }
+    public void Shot()
+    {
+        //particleSystem.Play();
+
+    }
+
+
+}
