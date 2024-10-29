3b3db391dca05f78f6e25c66cacd73556536b0a8
Provoda111 <provodochek2@gmail.com>
2024-10-29 10:21:07 +0200

    For alexey

--- Red-Jack/Assets/Scripts/World/Card/GameCard.cs
+++ Red-Jack/Assets/Scripts/World/Card/GameCard.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 public class GameCard : MonoBehaviour
 {
     [SerializeField] private int cardValue;
+    private bool isPointedAt;
+
+    private void Update()
+    {
+        if (isPointedAt)
+        {
+            Debug.Log("The card is watched at");
+        }
+    }
 }
--- Red-Jack/Assets/Scripts/World/Characters/Player.cs
+++ Red-Jack/Assets/Scripts/World/Characters/Player.cs
@@ -8,8 +8,23 @@ public class Player : MonoBehaviour
     private bool hasLost;
     [SerializeField] private List<GameObject> playerCards = new List<GameObject>();
     public List<BuffCard> buffCards = new List<BuffCard>();
+    private GameCard card;
+
+    [SerializeField] LayerMask layersToHit;
+
+    Ray ray;
+    RaycastHit hit;
+
 
     // Audio variables
 
+    private void Update()
+    {
+        ray = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(ray, out hit, 100, layersToHit))
+        {
+            Debug.Log($"Player's raycast has hit the {hit.collider.gameObject.name}");
+        }
+    }
 
 }
