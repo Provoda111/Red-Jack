4427b18c36958f13bd541833000e3f3a0202d02e
Provoda111 <provodochek2@gmail.com>
2024-11-12 09:19:38 +0200

    v 1110.1

--- Red-Jack/Assets/Scripts/World/Card/Game/GameCard.cs
+++ Red-Jack/Assets/Scripts/World/Card/Game/GameCard.cs
@@ -7,7 +7,7 @@ public class GameCard : MonoBehaviour
     [SerializeField] private int cardValue;
     private Animator cardAnimator;
     private Vector3 startPosition;
-
+    public GameObject targetPosition;
     public bool isAtTheTable = true;
 
     private void Start()
@@ -18,7 +18,7 @@ public class GameCard : MonoBehaviour
 
     private void LateUpdate()
     {
-        transform.position = new Vector3(startPosition.x, transform.position.y, startPosition.z); // Collider problem
+        //transform.position = new Vector3(startPosition.x, transform.position.y, startPosition.z); // Collider problem
         if (isAtTheTable) { UpdatePosition(); } // Collider problem
     }
     private void UpdatePosition()
@@ -41,18 +41,16 @@ public class GameCard : MonoBehaviour
     {
 
     }
-    public IEnumerator GoToCenter()
+    public void GoToCenter()
     {
-        GameObject targetPosition = GameObject.Find("CheckpointForCenter");
+        //targetPosition = GameObject.Find("CheckpointForCenter");
         
-        while (Vector3.Distance(transform.position, targetPosition.transform.position) < 0.01f)
+       /*while (Vector3.Distance(transform.position, targetPosition.transform.position) > 0.01f)
         {
-            transform.position = Vector3.Lerp(transform.position, targetPosition.transform.position, 2.0f * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, targetPosition.transform.position, 0.01f * Time.deltaTime);
             yield return null;
-        }
-        transform.position = targetPosition.transform.position;
-        targetPosition.transform.position = new Vector3(targetPosition.transform.position.x - 0.37296f, targetPosition.transform.position.y, targetPosition.transform.position.z);
-        //cardAnimator.SetTrigger("cardToTheCenter");
-        //yield return new WaitForSeconds(3.5f); | TEST IT #1
+        }*/
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition.transform.position, 0.01f);
+        targetPosition.transform.position = new Vector3(targetPosition.transform.position.x + 0.37296f, targetPosition.transform.position.y, targetPosition.transform.position.z);
     }
 }
--- Red-Jack/Assets/Scripts/World/GameManager.cs
+++ Red-Jack/Assets/Scripts/World/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> cardDeck = new List<GameObject>();
     public GameObject cardDeckObject;
+    public GameObject foo;
     [SerializeField] private GameCard card;
     public Animator deckAnimator;
     public Player player;
@@ -32,12 +33,16 @@ public class GameManager : MonoBehaviour
         for (int i = 0; i < 3; i++)
         {
             int cardIndex = Random.Range(0, cardDeck.Count);
-            GameCard selectedCard = cardDeck[cardIndex].GetComponent<GameCard>();
-            Debug.Log(cardIndex);
-            Instantiate(cardDeck[cardIndex], cardDeckObject.transform);
-            StartCoroutine(selectedCard.GoToCenter());
-            //selectedCard.StartCoroutine(GoToCenter()); | TEST IT #1
-            cardDeck.RemoveAt(cardIndex);
+            GameObject cardSpawner = GameObject.Find("CardSpawner");
+            GameObject cardObject = Instantiate(cardDeck[cardIndex], cardSpawner.transform);
+            GameCard card = cardObject.GetComponent<GameCard>();
+            card.targetPosition = GameObject.Find("CheckpointForCenter");
+            card.GoToCenter();
+            /*while (Vector3.Distance(card.gameObject.transform.position, card.targetPosition.transform.position) > 0.01f)
+            {
+                card.GoToCenter();
+            }*/
+            cardDeck.Remove(cardDeck[cardIndex]);
             yield return new WaitForSeconds(3.5f);
 
         }
