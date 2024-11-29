2d7b1f8281b09969170e69f41e7fd802a71f1a2d
s2200790 <s2200790@edu.bc.fi>
2024-11-29 12:07:18 +0200

    .

---
+++ Red-Jack/Assets/Scripts/World/Revolver/Revolver.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolver : MonoBehaviour
+{
+    int ShootQue = 1;
+    int WhereBullet;
+    private void Start()
+    {
+        WhereBullet = Random.Range(1, 7);
+        Debug.Log("Satunnainen luku: " + WhereBullet);
+    }
+
+    void TryToShot ()
+    {
+        if (ShootQue == WhereBullet)
+        {
+            Shoot();
+        }
+        else
+        {
+            MisFire();
+        }
+    }
+    void Shoot()
+    {
+
+    }
+    void MisFire()
+    {
+        ShootQue++;
+    }
+}
