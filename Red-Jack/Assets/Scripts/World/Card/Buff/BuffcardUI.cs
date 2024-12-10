2bd717f48869edf27f9fd8cb69b955e618a81902
Provoda111 <provodochek2@gmail.com>
2024-12-10 10:38:42 +0200

    Fix

--- Red-Jack/Assets/Scripts/World/Card/Buff/BuffcardUI.cs
+++ Red-Jack/Assets/Scripts/World/Card/Buff/BuffcardUI.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class BuffcardUI : MonoBehaviour
+public class BuffcardUI : BuffCard
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TextMeshProUGUI prefabName;
+    [SerializeField] private Sprite prefabImage;
+    [SerializeField] private TextMeshProUGUI prefabDesc;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        prefabName.text = buffCardName;
+        prefabImage = buffcardImage;
+        prefabDesc = buffCardDescription.ToString();
+    }
+    private void DrawCardOnMenu()
+    {
+
     }
 }
