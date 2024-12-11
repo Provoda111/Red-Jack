01a990083c4ebdf7a816c6ea2a0c4ce46934c0e3
s2200790 <s2200790@edu.bc.fi>
2024-12-11 12:51:12 +0200

    Over Scenes

--- Red-Jack/Assets/Scripts/World/Revolver/EnemyRevolver.cs
+++ Red-Jack/Assets/Scripts/World/Revolver/EnemyRevolver.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyRevolver : MonoBehaviour
 {
@@ -25,6 +26,7 @@ public class EnemyRevolver : MonoBehaviour
     void EnemyShoot()
     {
         blackjack.PlayerLost();
+        SceneManager.LoadScene("GameOver");
     }
     void EnemyMisFire()
     {
