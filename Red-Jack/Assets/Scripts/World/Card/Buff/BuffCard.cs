cf19013a37793f9ada81f0dce72ba05f6fbaccf3
Provoda111 <provodochek2@gmail.com>
2024-12-11 23:47:42 +0200

    updates to BuffCard.cs

--- Red-Jack/Assets/Scripts/World/Card/Buff/BuffCard.cs
+++ Red-Jack/Assets/Scripts/World/Card/Buff/BuffCard.cs
@@ -22,13 +22,36 @@ public class BuffCard : MonoBehaviour
     }
     internal void Spawn(GameObject caller)
     {
+        Gamer callerGamer = caller.GetComponent<Gamer>();
+
+        Gamer oppositeGamer = caller.name == "Player"
+            ? GameObject.Find("Enemy").GetComponent<Gamer>()
+            : GameObject.Find("Player").GetComponent<Gamer>();
         switch (buffType)
         {
             case buffCardType.Rubber:
+                // Poistetaan kortti kutsujan vastapuolelta
+                if (oppositeGamer.gameCards.Count > 0)
+                {
+                    GameObject cardToRemove = oppositeGamer.gameCards[0]; // Esimerkki: Poista ensimmäinen kortti
+                    oppositeGamer.RemoveCardFromSlot(cardToRemove);
+                }
                 break;
+
             case buffCardType.Add:
+                // Lisätään kortti vastapuolelle
+                GameObject newCardToAdd = callerGamer.deck.DrawCard(); // Luo/palauta uusi kortti pakasta
+                oppositeGamer.AddCardToSlot(newCardToAdd);
                 break;
+
             case buffCardType.Destroy:
+              
+                if (oppositeGamer.gameCards.Count > 0)
+                {
+                    GameObject cardToDestroy = oppositeGamer.gameCards[0]; 
+                    Destroy(cardToDestroy); 
+                    oppositeGamer.RemoveCardFromSlot(cardToDestroy); 
+                }
                 break;
         }
     }
